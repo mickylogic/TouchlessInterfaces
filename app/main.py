a6a90d5c6140d9a70fa41afe771adea797570cd2
Michael <mickmcferran.co.uk>
2020-06-24 16:34:08 +0100

    Changed to server side only

--- app/main.py
+++ app/main.py
@@ -2,7 +2,31 @@ from flask import Flask
 
 app = Flask(__name__) 
 
-@app.route("/") 
+#GPIO.setmode(GPIO.BCM)
+#done = False
+
+# original, edited for ngrok public hosting @app.route("/")
+@app.route("/", methods=['GET'])
 def home_view(): 
-		return "<h1>Welcome to Geeks for Geeks</h1>"
+        return "<h1>Welcome to GPIO data for my Pi</h1>"
+
+@app.route("/readPin/<pin>")
+def readPin(pin):
+   try:
+      GPIO.setup(int(pin), GPIO.IN)
+      if GPIO.input(int(pin)) == True:
+         response = "Pin number " + pin + " is high!"
+      elif GPIO.input(int(pin)) == False:
+         response = "Pin number " + pin + " is low!"
+   except:
+      response = "There was an error reading pin " + pin + "."
+
+   templateData = {
+      'title' : 'Status of Pin' + pin,
+      'response' : response
+      }
+
+
+
+   return render_template('pin.html', **templateData)
 
